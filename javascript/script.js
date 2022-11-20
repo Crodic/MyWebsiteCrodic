14e2874a92c12fd494c92ddf282536ed16c84f38
Crodic <113219638+Crodic@users.noreply.github.com>
2022-11-20 14:31:01 +0700

    first commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,11 @@
 function checkEmail(){
     var email = document.getElementById("email").value
+    var checkEmail=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     if (email !='' && email !=null){
-        confirm("Email: "+email +" đã đăng ký nhận thông báo thành công")
+        //Kiểm tra email đã đúng định dạng hay chưa
+        if(email.match(checkEmail)){
+            confirm("Email: "+email +" đã đăng ký nhận thông báo thành công")
+        }
     }else{
         alert("Email không hợp lệ vui lòng thử lại.")
     }
@@ -10,29 +14,32 @@ function submitOrder(){
     var name = document.getElementById("name").value
     var phone = document.getElementById("phone").value
     var emailOrder = document.getElementById("email-order").value
-    var listCheck1 = document.getElementById("list-check-1").value
-    var listCheck2 = document.getElementById("list-check-2").value
-    var listCheck3 = document.getElementById("list-check-3").value
     var order= document.getElementById("wish").value
-    var priceNumber=document.getElementById("number").value
-
-    if(name != '' && name != null){
-        if(!isNaN(phone) && phone !='' && phone !=null){
-            if(emailOrder !='' && emailOrder!=null){
-                confirm("Tên Khách Hàng: "+name
-                +" \n Số điện thoại: "+phone
-                +" \n Email: "+emailOrder
-                +" \n Số lượng: "+priceNumber
-                +" \n Yêu cầu: "+order
-                +" \n Nhấn OK để xác nhận thông tin")
+    var checkEmail=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if(name != '' && name != null && isNaN(name)==true){
+        if(isNaN(phone)==false && phone !='' && phone !=null && phone.length >= 10){
+            if(emailOrder!='' && emailOrder!=null){
+                if(emailOrder.match(checkEmail)){
+                    var orderCheck = prompt("Xác nhận lại số lượng mua hàng (Đây là kết quả cuối cùng)")
+                    confirm("Tên Khách Hàng: "+name
+                    +" \n Số điện thoại: "+phone
+                    +" \n Email: "+emailOrder
+                    +" \n Số lượng: "+orderCheck
+                    +" \n Yêu cầu: "+order
+                    +" \n Nhấn OK để xác nhận thông tin")
+                    alert("Anh/Chị "+name+" đã đặt hàng thành công. Trở lại trang chủ")
+                    window.location.href="./index.html"
+                }else{
+                    alert("Email không hợp lệ vui lòng thử lại.")
+                }
             }else{
-                alert("Email không hợp lệ vui lòng thử lại.")
+                alert("Vui Lòng nhập email")
             }
         }else{
             alert("Số điện thoại không hợp lệ")
         }
     }else{
-        alert("Thông tin mua hàng không hợp lệ.")
+        alert("Họ Tên không hợp lệ.")
     }
 }
 function sign() {
@@ -60,7 +67,7 @@ function sign() {
         document.getElementById('account-name').classList.remove('error')
     }
     //Kiểm tra mật khẩu
-    if(pass.length==0 || pass.length < 6 || pass !=checkPass){
+    if(pass.length==0 || pass.length < 6 || pass!=checkPass){
         document.getElementById('form-info_pass').innerHTML="Mật khẩu phải trên 6 ký tự"
         document.getElementById('password').classList.add('error')
     }else if(pass.length!=null && pass.length>6){
@@ -123,5 +130,34 @@ function remove() {
     }
     if(isSucess==true){
         document.getElementById('form-info_sucess').innerHTML=" "
+    }
+}
+function login(){
+    var accountName = document.getElementById('account-name').value
+    var pass = document.getElementById('password').value
+    var formLogin = document.getElementById('form-login')
+
+    if(accountName.length==0 || pass.length==0){
+        document.getElementById("account-name").classList.add('error')
+        document.getElementById("password").classList.add('error')
+        formLogin.innerHTML=" !!Tên đăng nhập hoặc mật khẩu không chính xác"
+        formLogin.style.color="red"
+        formLogin.style.fontSize="12px"
+    }else{
+        document.getElementById("account-name").classList.remove('error')
+        document.getElementById("password").classList.remove('error')
+        formLogin.innerHTML=" "
+        window.location.href="./index-sucess-login.html"
+    }
+}
+function removeLogin(){
+    var accountName = document.getElementById('account-name').value
+    var pass = document.getElementById('password').value
+
+    if(accountName.length !='' && accountName.length > 4){
+        document.getElementById('account-name').classList.remove('error')
+    }
+    if(pass.length!=null && pass.length > 6){
+        document.getElementById('password').classList.remove('error')
     }
 }
